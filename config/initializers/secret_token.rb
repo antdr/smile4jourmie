81b785ebb6fa87f521ea034bcf5e3735a6243fa2
Kuba Arndt <kuba.arndt@gmail.com>
2013-05-09 19:46:20 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smile4jourmie::Application.config.secret_token = '9963095bb4118c9e1de35ba211731f2141e7075c8bd7ea2719487ff51eb329b4aca4945ec1768a01ad017965e6ff851d1c40023824f506902023dbdb12e1d55b'
